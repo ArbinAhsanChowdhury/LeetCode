87c7e8ea32f796b40bbac03a0d7068f56c1483a8
ArbinAhsanChowdhury <mdarbinahsanchowdhury@gmail.com>
2026-07-04 20:10:27 +0600

    Add anagram checking function in problem_242.cpp
    Implement function to check if two strings are anagrams.

---
+++ problem_242.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    if(s == t){
+        return true;
+    }else{
+        return false;
+    }
+}
