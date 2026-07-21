b108113c44a621555f8ce2e640611dc628ac4d55
ArbinAhsanChowdhury <mdarbinahsanchowdhury@gmail.com>
2026-07-21 22:10:13 +0600

    Update problem_121.cpp

--- problem_121.cpp
+++ problem_121.cpp
@@ -1,41 +1,42 @@
-#include <bits/stdc++.h>
-using namespace std;    //code ta akono fully solved hoy nai akono onk chinta kora baki ase
-int main() {
-    vector<int> prices = {2,4,1};
-    vector<int> smallToBig;
-    int max_profit = 0;
-    int min_value = INT_MAX;
-    int min_index;
-    for(int i=0; i<prices.size(); ++i){
-        if(prices[i] < min_value){
-            min_value = prices[i];
-            min_index = i;
-        }
-    }
-    if(min_index + 1 == prices.size()){
-        smallToBig = prices;
-        sort(smallToBig.begin(), smallToBig.end());
-        
-        for(int i=smallToBig[1]; i<prices.size(); ++i){
-            if(max_profit < prices[i] - min_value){
-                max_profit = prices[i] - min_value;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //vector<int> prices = {2,4,1};
+        int max_profit = 0;
+        int min_value = INT_MAX;
+        int min_index;
+        //int loop=0;
+        for(int i=0; i<prices.size(); ++i){
+            if(prices[i] < min_value){
+                min_value = prices[i];
+                min_index = i;
             }
         }
-    }else{
+        //cout << min_value << " " << min_index+1 << " " << prices.size() <<endl;
+        
+        if(min_index+1 == prices.size()){
+            max_profit = 0;
+            min_value = INT_MAX;
+            for(int i=0; i<prices.size()-2; ++i){
+                if(prices[i] < min_value){ 
+                    min_value = prices[i];
+                    min_index = i;
+                    //loop++;
+                }
+            }
+        }
+        //cout << min_value << " " << min_index << " " << prices.size() << " " <<loop <<endl;
         for(int i=min_index; i<prices.size(); ++i){
             if(max_profit < prices[i] - min_value){
                 max_profit = prices[i] - min_value;
             }
         }
+        if(max_profit > 0 ){
+            //cout <<  max_profit;
+            return max_profit;
+        }else{
+            //cout <<  0;
+            return 0;
+        }
     }
-    cout << "size properti " << prices.size() << endl;
-    cout << "min_value " << min_value << endl;
-    cout << "min_index " << min_index << endl;
-    if(max_profit > 0 ){
-        cout << "max_profit " << max_profit << endl;
-    }else{
-        cout << "no max profit " << 0 << endl;
-    }
-    
-    return 0;
-}
+};
